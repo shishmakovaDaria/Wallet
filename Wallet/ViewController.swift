78353bc6dbb9e1c910fd62e8cb6ff2ce6d374904
Shishmakova Daria <dary_shishka@mail.ru>
2025-05-21 21:33:24 +0300

    Initial Commit

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by Дарья Шишмакова on 21.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

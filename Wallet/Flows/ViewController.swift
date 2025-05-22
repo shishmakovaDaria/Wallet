16c8d6627c4116428629fa25a02814c47ee5beb7
Shishmakova Daria <dary_shishka@mail.ru>
2025-05-22 11:02:58 +0300

    added snapkit

--- Wallet/Flows/ViewController.swift
+++ Wallet/Flows/ViewController.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        redView.snp.makeConstraints { make in
+            make.height.width.equalTo(100)
+            make.center.equalToSuperview()
+        }
     }
 }
